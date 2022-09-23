c2e68d8ef70799934999c5080d541bf4bcd673f9
daniscodes <dan97@bell.net>
2022-09-23 01:15:01 -0400

    Connecting user login

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,15 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
