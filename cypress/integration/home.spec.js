ce6369cf27bb00f2c0f1006504b89004f4834e7c
daniscodes <dan97@bell.net>
2022-09-28 00:26:45 -0400

    get cypress working, debugging tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe("Jungle", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  });
+
+  it("renders the home page when visited", () => {
+    cy.contains("Where you can find any plants!");
+  });
+});
+
+it("Confirms products are present", () => {
+  cy.get(".products article").should("have.length", 12);
+});
